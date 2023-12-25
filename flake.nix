446a3e99220050eaf09e2887a5f9be9179ba05d1
Tulili <tulilirockz@outlook.com>
2023-12-25 02:51:25 -0300

    feat: nix packaging and devshell for reproducible environment

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Local image building environment for Universal Blue systems";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; }; 
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+    in {
+      packages = {
+        zerolayer = mkPoetryApplication { projectDir = self; };
+        default = self.packages.${system}.zerolayer;
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = 
+          (with pkgs; [ nil python3 mypy ruff poetry ]) 
+          ++ 
+          (with pkgs.python311Packages; [ pip python-lsp-server pylsp-mypy python-lsp-ruff ]);
+      };
+    }
+  );
+}
+
