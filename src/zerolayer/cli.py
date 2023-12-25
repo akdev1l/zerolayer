dbac243f2f6fb8c7655664f72f3a407caadd78c7
Tulili <tulilirockz@outlook.com>
2023-12-25 04:04:00 -0300

    feat: list-images command for multiple archives

--- src/zerolayer/cli.py
+++ src/zerolayer/cli.py
@@ -5,6 +5,8 @@ import json
 import typer
 import logging
 import os
+import datetime
+import math
 
 app = typer.Typer()
 app_state = {"dry_run": False}
@@ -17,6 +19,17 @@ DEFAULT_PREFIX = "[ZEROLAYER]"
 DRY_RUN_PREFIX = "[DRY_RUN]"
 IMAGE_ARCHIVE_PATH = Path(f"{IMAGE_DIR}/current_image.tar.gz")
 CLEANUP_CMD = ["rm", "-rf", f"{IMAGE_DIR}/*"]
+CURRENT_ENVIRONMENT_NAME = "current"
+
+
+def convert_size(size_bytes):
+   if size_bytes == 0:
+       return "0B"
+   size_name = ("B", "KB", "MB", "GB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return "%s %s" % (s, size_name[i])
 
 
 def get_current_image() -> str:
@@ -25,52 +38,75 @@ def get_current_image() -> str:
 
 
 @app.command()
+def list_images(cache_dir: Path = IMAGE_DIR) -> None:
+    if not Path(cache_dir).exists():
+        logging.fatal("Failed to find image directory")
+        return
+
+    were_envs_found = False
+    # e.g.: boot_env.1.tar.gz boot_env.2.tar.gz boot_env.current.tar.gz
+    GENERIC_COOL_NAME_FOR_IMAGES: str = "boot_env"
+    for path in Path(cache_dir).iterdir():
+        full_file_name = path.name.split(".")
+        if GENERIC_COOL_NAME_FOR_IMAGES in full_file_name[0]:
+            if not full_file_name[1].isnumeric() and not full_file_name[1] == CURRENT_ENVIRONMENT_NAME:
+                logging.fatal(f"Invalid naming scheme for file {path.name}")
+                return
+            
+            logging.warning(f"Environment {full_file_name[1]}:\n\tSize: {convert_size(path.stat().st_size)}\n\tCreation Time: {datetime.datetime.fromtimestamp(path.stat().st_mtime)}")
+            were_envs_found = True
+
+    if not were_envs_found:
+        logging.warning("Could not find any valid environments")
+
+@app.command()
 def build_image(
         containerfile: Path = CONTAINERFILE_PATH,
-        output_dir: Path = IMAGE_DIR):
-    if (app_state["dry_run"]):
-        logging.info(f"{DRY_RUN_PREFIX} Create \"{IMAGE_DIR}\" and parent directories")  # noqa: E501
-    else:
-        logging.warning(f"{DEFAULT_PREFIX} Creating \"{IMAGE_DIR}\" and parent directories")  # noqa: E501
-        if (not output_dir.exists()):
-            try:
-                output_dir.mkdir(parents=True)
-            except PermissionError:
-                logging.fatal(f"Could not create {output_dir} due to permission errors. Are you not root?")  # noqa: E501
-                exit(1)
+        cache_dir: Path = IMAGE_DIR,
+        delete_cache: bool = False):
+    if app_state["dry_run"]:
+        logging.info(f"{DRY_RUN_PREFIX} Create \"{cache_dir}\" and parent directories")
+        logging.info(f"{DRY_RUN_PREFIX} Delete everything from {cache_dir}")
+        logging.info(f"{DRY_RUN_PREFIX} Create oci archive in {cache_dir} using {containerfile}")
+        exit(0)
 
-    if (app_state["dry_run"]):
-        logging.info(f"{DRY_RUN_PREFIX} Delete everything from {IMAGE_DIR}")
-    else:
+    if not cache_dir.exists():
+        logging.warning(f"{DEFAULT_PREFIX} Creating \"{cache_dir}\" and parent directories")
+        try:
+            cache_dir.mkdir(parents=True)
+        except PermissionError:
+            logging.fatal(f"Could not create {cache_dir} due to permission errors. Are you not root?")
+            exit(1)
+
+    if delete_cache:
         logging.warning(
-            f"{DEFAULT_PREFIX} Deleting everything from {IMAGE_DIR}")
+            f"{DEFAULT_PREFIX} Deleting everything from {cache_dir}")
         sp.run(CLEANUP_CMD)
 
-    if (app_state["dry_run"]):
-        logging.info(f"{DRY_RUN_PREFIX} Create oci archive in {output_dir} using {containerfile}")  # noqa: E501
-    else:
-        logging.warning(
-            f"{DEFAULT_PREFIX} Creating oci archive in {output_dir}")
-        sp.run(["buildah",
-                "bud",
-                "-t",
-                f"oci-archive:{output_dir}",
-                containerfile])
+    logging.warning(
+        f"{DEFAULT_PREFIX} Creating oci archive in {cache_dir}")
+    sp.run(["buildah",
+            "bud",
+            "-t",
+            f"oci-archive:{cache_dir}",
+            containerfile])
 
 
 @app.command()
-def rebase_to_image():
-    FULL_IMAGE: str = f"ostree-unverified-image:oci-archive:{IMAGE_ARCHIVE_PATH}"  # noqa: E501
+def rebase_to_image() -> None:
+    FULL_IMAGE: str = f"ostree-unverified-image:oci-archive:{IMAGE_ARCHIVE_PATH}"
+    
     if (app_state["dry_run"]):
         logging.info(f"{DRY_RUN_PREFIX} Rebase to {FULL_IMAGE}")
-    else:
-        logging.warning(f"{DEFAULT_PREFIX} Rebasing to {FULL_IMAGE}")
-        if (get_current_image() != FULL_IMAGE):
-            sp.run(["rpm-ostree", "rebase", FULL_IMAGE])
+        return
+    
+    logging.warning(f"{DEFAULT_PREFIX} Rebasing to {FULL_IMAGE}")
+    if (get_current_image() != FULL_IMAGE):
+        sp.run(["rpm-ostree", "rebase", FULL_IMAGE])
 
 
 @app.callback()
-def config(quiet: bool = False, dry_run: bool = False):
+def app_config(quiet: bool = False, dry_run: bool = False):
     logging.basicConfig(format="", level=logging.NOTSET)
 
     if quiet:
