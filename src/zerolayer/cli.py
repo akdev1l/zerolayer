0995a07c5d7458f58af50deeaee99ba2cbc554c1
Tulili <tulilirockz@outlook.com>
2023-12-26 02:43:48 -0300

    fix: minor fixes for edgecases and formatting

--- src/zerolayer/cli.py
+++ src/zerolayer/cli.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from pathlib import Path
-from rich.prompt import Prompt # type: ignore
-from rich.console import Console # type: ignore
-from rich.table import Table # type: ignore
+from rich.prompt import Prompt  # type: ignore
+from rich.console import Console  # type: ignore
+from rich.table import Table  # type: ignore
 from typing_extensions import Annotated
 import subprocess as sp
 import json
 import shutil
-import typer # type: ignore
+import typer  # type: ignore
 import logging
 import os
 import datetime
@@ -18,8 +18,9 @@ app = typer.Typer()
 app_state = {"dry_run": False}
 
 IMAGE_DIR = Path(os.environ.get("ZEROLAYER_IMAGE_DIR", "/var/cache/zerolayer"))
-CONTAINERFILE_PATH = Path(os.environ.get(
-    "ZEROLAYER_CONTAINERFILE_DIR", "/etc/zerolayer/Containerfile"))
+CONTAINERFILE_PATH = Path(
+    os.environ.get("ZEROLAYER_CONTAINERFILE_DIR", "/etc/zerolayer/Containerfile")
+)
 
 CYAN_COLOR = "\033[96m"
 BLUE_COLOR = "\033[94m"
@@ -31,18 +32,30 @@ CURRENT_ENVIRONMENT_NAME = "current"
 GENERIC_COOL_NAME_FOR_IMAGES: str = "boot_env"
 
 
+def get_valid_image_files(cache_dir: Path) -> list[Path]:
+    valid_files = []
+    for path in Path(cache_dir).iterdir():
+        full_file_name = path.name.split(".")
+        if GENERIC_COOL_NAME_FOR_IMAGES in full_file_name[0]:
+            if full_file_name[1] == CURRENT_ENVIRONMENT_NAME or path.is_dir():
+                continue
+
+            valid_files.append(path)
+    return valid_files
+
+
 def generate_hash_from_date(s: str):
-    return int(hashlib.sha256(s.encode('utf-8')).hexdigest(), 16) % 10**8
+    return int(hashlib.sha256(s.encode("utf-8")).hexdigest(), 16) % 10**8
 
 
 def convert_size(size_bytes):
-   if size_bytes == 0:
-       return "0B"
-   size_name = ("B", "KB", "MB", "GB")
-   i = int(math.floor(math.log(size_bytes, 1024)))
-   p = math.pow(1024, i)
-   s = round(size_bytes / p, 2)
-   return "%s %s" % (s, size_name[i])
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
 
 
 def get_current_image() -> str:
@@ -51,70 +64,79 @@ def get_current_image() -> str:
 
 
 @app.command()
-def list_environments(cache_dir: Path = IMAGE_DIR, max_shown: int = 0, ignore_current: bool = False) -> None:
+def list_environments(
+    cache_dir: Path = IMAGE_DIR, max_shown: int = 0, ignore_current: bool = False
+) -> None:
     if not Path(cache_dir).exists():
-        logging.fatal("Failed to find image directory")
+        logging.fatal(f"{DEFAULT_PREFIX} Failed to find image directory")
         return
 
     table_list = Table("Filename", "Hash", "Size", "Creation Time")
     envs_found = 0
-    # e.g.: boot_env.1HASH.tar.gz boot_env.2HASH.tar.gz boot_env.current.tar.gz
+
     for path in Path(cache_dir).iterdir():
         if envs_found == max_shown and max_shown != 0:
-                return
+            break
         full_file_name = path.name.split(".")
         if GENERIC_COOL_NAME_FOR_IMAGES in full_file_name[0]:
-            if not full_file_name[1].isnumeric() and not full_file_name[1] == CURRENT_ENVIRONMENT_NAME:
-                logging.fatal(f"Invalid naming scheme for file {path.name}")
-                return
-            
+            if path.is_dir():
+                continue
+
             if ignore_current and full_file_name[1] == CURRENT_ENVIRONMENT_NAME:
                 envs_found += 1
                 continue
-            
-            table_list.add_row(path.name, full_file_name[1], convert_size(path.stat().st_size), str(datetime.datetime.fromtimestamp(path.stat().st_mtime)))
+
+            table_list.add_row(
+                path.name,
+                full_file_name[1],
+                convert_size(path.stat().st_size),
+                str(datetime.datetime.fromtimestamp(path.stat().st_mtime)),
+            )
 
             envs_found += 1
-    
+
     if envs_found == 0:
         logging.warning("Could not find any valid environments")
         return
-    
+
     Console().print(table_list)
 
 
 @app.command()
-def clear(cache_dir: Path = IMAGE_DIR, all: bool = False, no_confirm: bool = False, list_size: int = 0):
+def clear(
+    cache_dir: Path = IMAGE_DIR,
+    all: bool = False,
+    no_confirm: bool = False,
+    list_size: int = 0,
+):
     if app_state["dry_run"] and no_confirm is False:
         logging.info(f"{DRY_RUN_PREFIX} Delete everything from {cache_dir}")
         exit(0)
 
     if not Path(cache_dir).exists():
-        logging.fatal("Failed to find image directory")
+        logging.fatal(f"{DEFAULT_PREFIX} Failed to find image directory")
         return
 
-    valid_files: list[Path] = [] 
-    for path in Path(cache_dir).iterdir():
-        full_file_name = path.name.split(".")
-        if GENERIC_COOL_NAME_FOR_IMAGES in full_file_name[0]:
-            if full_file_name[1] == CURRENT_ENVIRONMENT_NAME:
-                continue
-    
-            valid_files.append(path)
-
+    valid_files: list[Path] = get_valid_image_files(cache_dir)
     if valid_files == []:
-        logging.warning("Could not find any environment")
+        logging.warning(f"{DEFAULT_PREFIX} Could not find any environment")
         return
 
     if all:
-        logging.warning("Affected environments:\n\t" + "\n\t".join([file.name for file in valid_files]) + "\n")
-        
-        are_you_sure: bool = False 
+        logging.warning(
+            f"{DEFAULT_PREFIX} Affected environments:\n\t"
+            + "\n\t".join([file.name for file in valid_files])
+            + "\n"
+        )
+
+        are_you_sure: bool = False
         if no_confirm:
             are_you_sure = True
         else:
-            are_you_sure = typer.confirm("Are you sure you want to delete all environments?", abort=True)
-   
+            are_you_sure = typer.confirm(
+                "Are you sure you want to delete all environments?", abort=True
+            )
+
         if not are_you_sure:
             raise typer.Abort()
 
@@ -122,24 +144,28 @@ def clear(cache_dir: Path = IMAGE_DIR, all: bool = False, no_confirm: bool = Fal
             path.unlink()
         return
 
-
     list_environments(cache_dir, list_size, ignore_current=True)
     if list_size > 0:
-        logging.warning(f"Results truncated to {list_size} results.")
+        logging.warning(f"{DEFAULT_PREFIX} Results truncated to {list_size} results.")
 
-    selected_env: str = Prompt.ask("\nWhich environment do you want to delete?", choices=[str(x.name.split(".")[1]) for x in valid_files])
+    selected_env: str = Prompt.ask(
+        "\nWhich environment do you want to delete?",
+        choices=[str(x.name.split(".")[1]) for x in valid_files],
+    )
 
-    r_you_sure: bool = False 
+    r_you_sure: bool = False
     if no_confirm:
         r_you_sure = True
     else:
-        r_you_sure = typer.confirm("Are you sure you want to delete the selected environment?", abort=True)
-   
+        r_you_sure = typer.confirm(
+            "Are you sure you want to delete the selected environment?", abort=True
+        )
+
     if not r_you_sure:
         raise typer.Abort()
 
-    logging.warning("Deleting selected environment")
-    
+    logging.warning(f"{DEFAULT_PREFIX} Deleting selected environment")
+
     for file in valid_files:
         if file.name.split(".")[1] == selected_env:
             try:
@@ -149,30 +175,36 @@ def clear(cache_dir: Path = IMAGE_DIR, all: bool = False, no_confirm: bool = Fal
                 exit(1)
             logging.warning("Environment deleted successfully")
             return
-    
+
     logging.warning("Could not delete selected environment")
     exit(1)
 
 
 @app.command()
 def build(
-        containerfile: Path = CONTAINERFILE_PATH,
-        cache_dir: Path = IMAGE_DIR,
-        build_arg: Annotated[list[str], typer.Option()] = []
-    ):
+    containerfile: Path = CONTAINERFILE_PATH,
+    cache_dir: Path = IMAGE_DIR,
+    build_arg: Annotated[list[str], typer.Option()] = [],
+):
     if app_state["dry_run"]:
-        logging.info(f"{DRY_RUN_PREFIX} Create \"{cache_dir}\" and parent directories")
-        logging.info(f"{DRY_RUN_PREFIX} Create oci archive in {cache_dir} using {containerfile}")
+        logging.info(f'{DRY_RUN_PREFIX} Create "{cache_dir}" and parent directories')
+        logging.info(
+            f"{DRY_RUN_PREFIX} Create oci archive in {cache_dir} using {containerfile}"
+        )
         logging.info(f"{DRY_RUN_PREFIX} Unlinking current environment")
         logging.info(f"{DRY_RUN_PREFIX} Symlinking generated file to current")
-        return 
+        return
 
     if not cache_dir.exists():
-        logging.warning(f"{DEFAULT_PREFIX} Creating \"{cache_dir}\" and parent directories")
+        logging.warning(
+            f'{DEFAULT_PREFIX} Creating "{cache_dir}" and parent directories'
+        )
         try:
             cache_dir.mkdir(parents=True)
         except PermissionError:
-            logging.fatal(f"Could not create {cache_dir} due to permission errors. Are you not root?")
+            logging.fatal(
+                f"Could not create {cache_dir} due to permission errors. Are you not root?"
+            )
             exit(1)
         except OSError:
             logging.fatal(f"Failed creating {cache_dir}")
@@ -180,38 +212,51 @@ def build(
 
     TARGET_FILE_NAME = f"{cache_dir.resolve()}/{GENERIC_COOL_NAME_FOR_IMAGES}.{generate_hash_from_date(str(datetime.datetime.now()))}.tar"
 
-    logging.warning(
-        f"{DEFAULT_PREFIX} Creating oci archive in {cache_dir}")
-    
+    logging.warning(f"{DEFAULT_PREFIX} Creating oci archive in {cache_dir}")
+
     build_args: list[str] = []
-    if build_arg != []: 
+    if build_arg != []:
         build_args = ["--build-arg=" + arg for arg in build_arg]
-    
-    sp.run(["buildah",
-            "bud",
-            "-o"] 
-            + build_args +
-            [f"type=tar,dest={TARGET_FILE_NAME}",
-            containerfile])
+
+    try:
+        buildah_run = sp.run(
+            ["buildah", "bud", "-o"]
+            + build_args
+            + [f"type=tar,dest={TARGET_FILE_NAME}", containerfile]
+        )
+    except FileNotFoundError:
+        logging.fatal(
+            f"{DEFAULT_PREFIX} Failed to run Buildah, do you have it in your PATH?"
+        )
+        exit(1)
+
+    if buildah_run.returncode != 0:
+        logging.fatal(f"{DEFAULT_PREFIX} ")
+        exit(1)
 
     logging.warning(f"{DEFAULT_PREFIX} Unlinking current environment")
     for file in Path(cache_dir).iterdir():
         full_file_name = file.name.split(".")
-        if GENERIC_COOL_NAME_FOR_IMAGES in full_file_name[0] and full_file_name[1] == CURRENT_ENVIRONMENT_NAME:
-            file.unlink()
+        if (
+            GENERIC_COOL_NAME_FOR_IMAGES in full_file_name[0]
+            and full_file_name[1] == CURRENT_ENVIRONMENT_NAME
+        ):
+            file.unlink(missing_ok=True)
 
     logging.warning(f"{DEFAULT_PREFIX} Symlinking generated file to current")
-    Path(f"{cache_dir}/{GENERIC_COOL_NAME_FOR_IMAGES}.{CURRENT_ENVIRONMENT_NAME}.tar").symlink_to(TARGET_FILE_NAME)
+    Path(
+        f"{cache_dir}/{GENERIC_COOL_NAME_FOR_IMAGES}.{CURRENT_ENVIRONMENT_NAME}.tar"
+    ).symlink_to(TARGET_FILE_NAME)
 
 
 @app.command()
 def rebase(cache_dir: Path = IMAGE_DIR) -> None:
     FULL_IMAGE: str = f"ostree-unverified-image:oci-archive:{cache_dir}/{GENERIC_COOL_NAME_FOR_IMAGES}.{CURRENT_ENVIRONMENT_NAME}.tar"
-    
+
     if app_state["dry_run"]:
         logging.info(f"{DRY_RUN_PREFIX} Rebase to {FULL_IMAGE}")
         return
-    
+
     logging.warning(f"{DEFAULT_PREFIX} Rebasing to {FULL_IMAGE}")
     if get_current_image() != FULL_IMAGE:
         sp.run(["rpm-ostree", "rebase", FULL_IMAGE])
@@ -219,9 +264,9 @@ def rebase(cache_dir: Path = IMAGE_DIR) -> None:
 
 @app.command()
 def init(
-        url: str = "https://github.com/ublue-os/startingpoint", 
-        target_dir: Path = CONTAINERFILE_PATH.parents[0], 
-        no_confirm: bool = False
+    url: str = "https://github.com/ublue-os/startingpoint",
+    target_dir: Path = CONTAINERFILE_PATH.parents[0],
+    no_confirm: bool = False,
 ):
     if app_state["dry_run"]:
         logging.info(f"{DRY_RUN_PREFIX} Delete everything from {target_dir}")
@@ -229,27 +274,32 @@ def init(
         return
 
     if target_dir.exists() and len(os.listdir(target_dir)) > 0:
-        are_you_sure: bool = False 
+        are_you_sure: bool = False
         if no_confirm:
             are_you_sure = True
         else:
-            are_you_sure = typer.confirm(f"Are you sure you want to delete everything from {target_dir}", abort=True)
+            are_you_sure = typer.confirm(
+                f"Are you sure you want to delete everything from {target_dir}",
+                abort=True,
+            )
 
         if not are_you_sure:
             return
 
         deleted_files = [str(file) for file in target_dir.iterdir()]
-        
+
         shutil.rmtree(target_dir)
 
-        logging.warning(f"{DEFAULT_PREFIX} Affected files:\n\t" + "\n\t".join(deleted_files) + "\n")
-    
+        logging.warning(
+            f"{DEFAULT_PREFIX} Affected files:\n\t" + "\n\t".join(deleted_files) + "\n"
+        )
+
     try:
-        gitclone = sp.run(["git","clone",url,str(target_dir)])
+        gitclone = sp.run(["git", "clone", url, str(target_dir)])
     except FileNotFoundError:
         logging.fatal(f"{DEFAULT_PREFIX} Could not run Git, is it in your PATH?")
         exit(1)
-    
+
     if gitclone.returncode != 0:
         logging.fatal(f"{DEFAULT_PREFIX} Failed to initialize in {target_dir}")
         exit(1)
@@ -258,34 +308,30 @@ def init(
 
 
 @app.command()
-def switch(cache_dir: Path = IMAGE_DIR,image_hash: str = ""):
+def switch(cache_dir: Path = IMAGE_DIR, image_hash: str = ""):
     if app_state["dry_run"]:
         logging.info(f"{DRY_RUN_PREFIX} List and choose environments")
         logging.info(f"{DRY_RUN_PREFIX} Unlink old current environment")
         logging.info(f"{DRY_RUN_PREFIX} Link selected environment to current")
         return
 
-    valid_files: list[Path] = [] 
-    for path in Path(cache_dir).iterdir():
-        full_file_name = path.name.split(".")
-        if GENERIC_COOL_NAME_FOR_IMAGES in full_file_name[0]:
-            if full_file_name[1] == CURRENT_ENVIRONMENT_NAME:
-                continue
-    
-            valid_files.append(path)
-
+    valid_files: list[Path] = get_valid_image_files(cache_dir)
     if valid_files == []:
-        logging.warning("Could not find any environment")
+        logging.warning(f"{DEFAULT_PREFIX} Could not find any environment")
         return
 
-    
     if image_hash != "":
         selected_hash = image_hash
     else:
         list_environments(cache_dir, 0, ignore_current=True)
-        selected_hash = Prompt.ask("\nWhich environment do you want to switch to?", choices=[str(x.name.split(".")[1]) for x in valid_files])
+        selected_hash = Prompt.ask(
+            "\nWhich environment do you want to switch to?",
+            choices=[str(x.name.split(".")[1]) for x in valid_files],
+        )
 
-    CURRENT_ENV_FILE = Path(f"{cache_dir}/{GENERIC_COOL_NAME_FOR_IMAGES}.{CURRENT_ENVIRONMENT_NAME}.tar")   
+    CURRENT_ENV_FILE = Path(
+        f"{cache_dir}/{GENERIC_COOL_NAME_FOR_IMAGES}.{CURRENT_ENVIRONMENT_NAME}.tar"
+    )
 
     if CURRENT_ENV_FILE.exists():
         logging.warning(f"{DEFAULT_PREFIX} Unlinking old current symlink")
@@ -298,13 +344,15 @@ def switch(cache_dir: Path = IMAGE_DIR,image_hash: str = ""):
             found_env = True
             target_env = file
 
-    if found_env:
-        CURRENT_ENV_FILE.symlink_to(target_env)
-        proper_final_hash = target_env.name.split(".")[1]
-        logging.warning(f"{DEFAULT_PREFIX} Switched current environment to {proper_final_hash}")
-    else:
+    if not found_env:
         logging.fatal(f"{DEFAULT_PREFIX} Failed to find environment")
         exit(1)
+
+    CURRENT_ENV_FILE.symlink_to(target_env)
+    proper_final_hash = target_env.name.split(".")[1]
+    logging.warning(
+        f"{DEFAULT_PREFIX} Switched current environment to {proper_final_hash}"
+    )
 
 
 @app.callback()
